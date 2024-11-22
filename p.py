3a313129214307fb3e7104817cf9bab09dbc4224
2305a21l42 <2305a21l42@sru.edu.in>
2024-11-22 14:19:36 +0530

    Add files via upload

--- p.py
+++ p.py
@@ -9,11 +9,12 @@ def Elec_Para(V, R, T):
 
 # Set up the Streamlit application title
 st.title("2305A21L42-PS1")  # Replace with your own Roll No. and Problem Statement No.
-
+st.write("This Application is useful for calculating current through a load,power drawn by the load,and heat energy generated")
 # Add inputs for Voltage (V), Resistance (R), and Time (T)
 V = st.number_input("Enter Voltage (V) in volts", value=230.0)
 R = st.number_input("Enter Resistance (R) in ohms", value=10.0)
 T = st.number_input("Enter Time (T) in seconds", value=60)
+st.button("compute")
 
 # Call the Elec_Para function to calculate I, P, and H
 I, P, H = Elec_Para(V, R, T)
